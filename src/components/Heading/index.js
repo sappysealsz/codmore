f04293c3ac4d16f0888b2e327d865a966556d972
sappysealsz <enchantercode@gmail.com>
2022-08-07 01:15:52 -0500

    [HEADING]

--- src/components/Heading/index.js
+++ src/components/Heading/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Heading = () => {
   return (
     <>
-      <section className="px-8 py-4 inline-flex flex-col custom-container w-1/4 bottom-12 right-20">
+      <section className="px-8 py-4 inline-flex flex-col custom-container w-4/5 md:w-2/4 lg:w-2/5 bottom-12 right-10 md:right-20">
         <div className="text-purple-50 mb-2 flex flex-col items-center">
           <h1 className="text-center font-bold text-4xl">Web3 & Metaverso</h1>
           <p className="text-lg font-thin italic ">
